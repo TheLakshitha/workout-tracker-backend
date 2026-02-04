cbbd946b46db428a123639b654a8da1d992b40e4
Ravindu Wijesundara <ravilakshitha911@gmail.com>
2026-02-04 12:49:30 +0530

    Mongo Comment

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 4000
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
+        console.log("MongoDB connected ✅");
         app.listen(PORT, () => {
             console.log(`listeneing on port ${PORT}`);
         });
