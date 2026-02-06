8fc9ceef16efac17435e4147043225bc3febb3f3
Ravindu Wijesundara <ravilakshitha911@gmail.com>
2026-02-06 14:44:35 +0530

    Fix CORS for Vercel frontend

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://YOUR_FRONTEND_DOMAIN_HERE'
+    'https:/workout-tracker-frontend-silk.vercel.app'
   ],
   credentials: true
 }))                          // ✅ ALLOWS ALL ORIGINS FOR NOW
